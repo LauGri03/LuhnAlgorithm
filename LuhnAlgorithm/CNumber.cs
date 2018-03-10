baa62c44617964e172149a0a478a97fd3119104f
Lolo <Lolo@CHOCHO>
2018-03-09 22:27:35 -0500

    Ajoutez des fichiers projet.

---
+++ LuhnAlgorithm/CNumber.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuhnAlgorithm
+{
+    class CNumber 
+    {
+
+        int[] Tab = null;
+
+        public CNumber(string Enter){
+            Tab = new int[Enter.Length];
+
+            for (int i = 0; i < Enter.Length; i++){
+                    Tab[i] = Convert.ToInt32(Enter.Substring(i, 1));
+               
+            }
+        }
+
+       
+        public bool Verify(){
+            int key = 0;
+
+            for (int i = Tab.Length - 2; i != 1; i--)
+            {
+                if ((i % 2 == 0)){
+                    if (Tab[i] * 2 >= 10)
+                        key += (Tab[i] * 2 - 9);
+                    else
+                        key += Tab[i] * 2;
+                }
+                else{
+                    key += Tab[i];
+                }
+                    
+            }
+
+            if (key % 10 == 0)
+                return true;
+            else
+                return false;
+                
+        }
+
+        
+    }
+}
---
+++ LuhnAlgorithm/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuhnAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CNumber Nombre = null;
+            do
+            {
+                Console.WriteLine("Entrez une serie de chiffres");
+                string ok = Console.ReadLine();
+                Nombre = new CNumber(ok);
+                Console.WriteLine(Nombre.Verify());
+
+            } while (true);
+        }
+    }
+}
